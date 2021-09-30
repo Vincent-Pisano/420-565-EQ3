1a66e65c93761a9353a5c30806711a7354665189
Pravdin Maksym <maks111111112@gmail.com>
2021-09-30 18:05:40 -0400

    EQ3-7 Ajout de profile dans Home et ajout de selection de pdf pour deposer un CV pour les etudiants

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,57 @@
 import React from "react";
 import auth from "../services/Auth";
 import "../App.css"
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import pfp from './../assets/img/pfp.png';
+import CV from "../components/CV/CV"
+import "./Home.css"
 
 function Home() {
 
   let user = auth.user
 
+  let dateFormat = formatDate(user.creationDate)
+
+  function formatDate(dateString) {
+    let date = new Date(dateString);
+    let dateFormatted = date.toISOString().split("T")[0];
+    return dateFormatted;
+  }
+
+  function checkIfStudent() {
+    if (user.username.startsWith("E")) {
+      return (
+        <>
+          <CV />
+        </>
+      );
+    }
+  }
+  
   return (
     <Container className="cont_home">
-      <h1>Bonjour {user.firstName} !</h1>
+      <Row className="cont_central">
+        <Col md="auto" className="cont_form">
+          <h2>Bonjour {user.firstName}!</h2>
+          <Row>
+            <Card bg="secondary" text="white" className="pfp_card">
+              <br />
+              <Card.Img variant="top" src={pfp}/>
+              <Card.Body>
+                <Card.Title>
+                  <h4>Nom d'utilisateur: {user.username}</h4>
+                </Card.Title>
+                <h5>Prénom: {user.firstName}</h5>
+                <h5>Nom: {user.lastName}</h5>
+                <h5>Adresse courriel: {user.email}</h5>
+                <h5>Depuis {dateFormat}</h5>
+              </Card.Body>
+              {checkIfStudent()}
+              <br />
+            </Card>
+          </Row>
+        </Col>
+      </Row> 
     </Container>
   );
 }
