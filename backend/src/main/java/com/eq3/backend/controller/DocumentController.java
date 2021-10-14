695a1b8fb2033124c44874fb3aa98a5b42bc7578
Renaud Vincent <v.pisano35@gmail.com>
2021-10-14 16:28:50 -0400

    EQ3-16 Ajout début gestion de document

---
+++ backend/src/main/java/com/eq3/backend/controller/DocumentController.java
@@ -0,0 +1,9 @@
+package com.eq3.backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3006")
+public class DocumentController {
+}
---
+++ backend/src/main/java/com/eq3/backend/service/DocumentService.java
@@ -0,0 +1,45 @@
+package com.eq3.backend.service;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocumentService {
+
+    public void updateDocument(String input, String output)
+            throws IOException {
+
+        try (XWPFDocument doc = new XWPFDocument(
+                Files.newInputStream(Paths.get(input)))
+        ) {
+
+            List<XWPFParagraph> xwpfParagraphList = doc.getParagraphs();
+            //Iterate over paragraph list and check for the replaceable text in each paragraph
+            for (XWPFParagraph xwpfParagraph : xwpfParagraphList) {
+                for (XWPFRun xwpfRun : xwpfParagraph.getRuns()) {
+                    String docText = xwpfRun.getText(0);
+                    //replacement and setting position
+                    docText = docText.replace("${name}", "San Pellegrino");
+                    xwpfRun.setText(docText, 0);
+                }
+            }
+
+            // save the docs
+            try (FileOutputStream out = new FileOutputStream(output)) {
+                doc.write(out);
+            }
+
+        }
+
+    }
+
+}
