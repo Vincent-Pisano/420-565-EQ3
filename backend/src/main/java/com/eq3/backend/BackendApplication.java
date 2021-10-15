8fdd4853ff9d3ff780f4abc31fe75e13b37903c8
Renaud Vincent <v.pisano35@gmail.com>
2021-10-15 10:19:56 -0400

    EQ3-16 Création méthode service saveInternship

--- backend/src/main/java/com/eq3/backend/BackendApplication.java
+++ backend/src/main/java/com/eq3/backend/BackendApplication.java
@@ -1,18 +1,34 @@
 package com.eq3.backend;
 
+import com.eq3.backend.model.InternshipApplication;
+import com.eq3.backend.repository.InternshipApplicationRepository;
+import com.eq3.backend.service.InternshipService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
+
+    private InternshipService internshipService;
+    private InternshipApplicationRepository internshipApplicationRepository;
+
+    public BackendApplication(InternshipService internshipService,
+                              InternshipApplicationRepository internshipApplicationRepository) {
+        this.internshipService = internshipService;
+        this.internshipApplicationRepository = internshipApplicationRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
+    
     public void run(String... args) throws Exception {
-
+        Optional<InternshipApplication> optionalInternshipApplication
+                = internshipApplicationRepository.findById("6168a6bcce3fbc6d36fdcd34");
+        optionalInternshipApplication
+                .ifPresent(internshipApplication -> internshipService.saveInternship(internshipApplication));
     }
-
 }
--- backend/src/main/java/com/eq3/backend/service/InternshipService.java
+++ backend/src/main/java/com/eq3/backend/service/InternshipService.java
@@ -22,15 +22,18 @@ public class InternshipService {
     private final StudentRepository studentRepository;
     private final InternshipOfferRepository internshipOfferRepository;
     private final InternshipApplicationRepository internshipApplicationRepository;
+    private final InternshipRepository internshipRepository;
 
     InternshipService(StudentRepository studentRepository,
                    InternshipOfferRepository internshipOfferRepository,
-                   InternshipApplicationRepository internshipApplicationRepository
+                   InternshipApplicationRepository internshipApplicationRepository,
+                   InternshipRepository internshipRepository
     ) {
         this.logger = LoggerFactory.getLogger(BackendService.class);
         this.studentRepository = studentRepository;
         this.internshipOfferRepository = internshipOfferRepository;
         this.internshipApplicationRepository = internshipApplicationRepository;
+        this.internshipRepository = internshipRepository;
     }
 
     public Optional<InternshipOffer> saveInternshipOffer(String internshipOfferJson, MultipartFile multipartFile) {
@@ -110,5 +113,16 @@ public class InternshipService {
 
         return optionalInternshipApplication.map((_internshipApplication) ->
                 internshipApplicationRepository.save(internshipApplication));
+    }
+
+    public Optional<Internship> saveInternship(InternshipApplication internshipApplication) {
+        Internship internship = new Internship();
+        internship.setInternshipApplication(internshipApplication);
+        internship.setInternshipContract(getContract(internshipApplication));
+        return Optional.of(internshipRepository.save(internship));
+    }
+
+    private PDFDocument getContract(InternshipApplication internshipApplication) {
+        return null;
     }
 }
