6dd61af51757608cf5df36898d33567895de3048
Jules Emond <julesemond@outlook.com>
2021-10-19 12:12:14 -0400

    Modification des services

--- veille/.eslintrc.js
+++ veille/.eslintrc.js
@@ -3,13 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    'eslint:recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
