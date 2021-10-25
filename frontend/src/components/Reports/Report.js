d87ed84b6432a98c7683d0d09e37b5f5223b49e1
Pravdin Maksym <maks111111112@gmail.com>
2021-10-25 17:21:22 -0400

    EQ3-175 Ajout de reportsList dans le frontend

---
+++ frontend/src/components/Reports/Report.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Row, Col } from "react-bootstrap";
+import { useHistory } from "react-router";
+
+const Report = ({ report }) => {
+  let history = useHistory();
+
+  function onCreatePost(e) {
+    e.preventDefault();
+    history.push({
+      pathname: `/reports/${report.link}`,
+    });
+  }
+
+  return (
+    <Row className="list_node" onClick={(e) => onCreatePost(e)}>
+      <Col xs={3}>
+        <FontAwesomeIcon className="fa-3x" icon={report.icon} />
+      </Col>
+      <Col xs={9} className="list_node_text">
+        <li>{report.title}</li>
+      </Col>
+    </Row>
+  );
+};
+
+export default Report;
---
+++ frontend/src/components/Reports/ReportsHome.js
@@ -0,0 +1,24 @@
+import { Container } from "react-bootstrap";
+
+import Report from "./Report";
+import { ReportsList } from "../../Utils/Reports";
+
+const ReportsHome = () => {
+  let reportsList = ReportsList();
+
+  return (
+    <Container className="cont_principal">
+      <Container className="cont_list_centrar">
+        <h2 className="cont_title_form">Les rapports disponibles:</h2>
+        <Container className="cont_list">
+          <ul>
+            {reportsList.map((report) => (
+              <Report key={report.id} report={report} />
+            ))}
+          </ul>
+        </Container>
+      </Container>
+    </Container>
+  );
+};
+export default ReportsHome;
