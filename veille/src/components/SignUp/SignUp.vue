ef263cc8f12ec2796cf0711605e6c1480ec3cd21
Jules Emond <julesemond@outlook.com>
2021-10-31 14:55:01 -0400

    Création et fonctionnement de SignUp.vue

---
+++ veille/src/components/SignUp/SignUp.vue
@@ -0,0 +1,41 @@
+<script>
+import SignUpStudent from './SignUpStudent.vue'
+export default{
+        name: 'signUp',
+        data: function(){
+            return { currentSignUp: 'SignUpStudent'}
+        },
+        components: {
+            SignUpStudent
+        },
+        methods: {
+            getCurrentSignUp(){
+                return this.currentSignUp
+            },
+            setCurrentSignUp(newValue){
+                this.currentSignUp = newValue
+            },
+            handleClick(newChoice){
+                if(newChoice === "student"){
+                    this.setCurrentSignUp('SignUpStudent')
+                    return this.currentSignUp
+                }
+            }
+        }
+    }
+
+</script>
+
+<style scoped src="@/styles/Form.css"></style>
+
+<template>
+    <div id="signUp" class="cont_principal">
+      <div class="cont_central">
+        <h2 class="cont_title_form">Inscription</h2>
+          <div class="cont_buttons_sign_up">
+              <button :class = "getCurrentSignUp()==='SignUpStudent' ? 'btn_link_selected': 'btn_link'" v-on:click="handleClick('student')">Étudiant</button>
+              <component v-bind:is="getCurrentSignUp()"></component>
+            </div>
+        </div>
+    </div>
+</template>
