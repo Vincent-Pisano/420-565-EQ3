7c55b94c034d9875da80fda56350f6b75b4f1cff
Pravdin Maksym <maks111111112@gmail.com>
2021-11-01 17:22:37 -0400

    Veille_Maksym Ajout de component Button et Header

---
+++ vue/vue-frontend/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: "Hello"
+        }
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
