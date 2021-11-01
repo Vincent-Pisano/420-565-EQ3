987c479743fc4409f27f5a29bbe973fe925df9c6
Pravdin Maksym <maks111111112@gmail.com>
2021-11-01 19:03:28 -0400

    Veille_Maksym Ajout de addTasks

--- vue/vue-frontend/src/App.vue
+++ vue/vue-frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" class="container">
     <h1>Page Home</h1>
     <Header title="Task Tracker"/>
+    <AddTask @add-task="addTask" />
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
@@ -10,12 +11,14 @@
 
 import Header from './components/Header.vue'
 import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask,
   },
   data() {
     return {
@@ -23,6 +26,9 @@ export default {
     }
   },
   methods: {
+    addTask(task){
+      this.tasks = [...this.tasks, task]
+    },
     deleteTask(id){
       if(confirm('Are you sure?')){
         this.tasks = this.tasks.filter((task) => task.id !==
