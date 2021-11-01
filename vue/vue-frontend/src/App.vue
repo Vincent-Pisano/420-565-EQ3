f06daf2e15f2f4b5d77210eae6afbe37db8ef5c2
Pravdin Maksym <maks111111112@gmail.com>
2021-11-01 19:15:17 -0400

    Veille_Maksym Ajout de conditions sur les boutons

--- vue/vue-frontend/src/App.vue
+++ vue/vue-frontend/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app" class="container">
     <h1>Page Home</h1>
-    <Header title="Task Tracker"/>
-    <AddTask @add-task="addTask" />
+    <Header @toggle-add-task="toggleAddTask" :showAddTask="showAddTask" title="Task Tracker"/>
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
@@ -22,10 +24,14 @@ export default {
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false
     }
   },
   methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
     addTask(task){
       this.tasks = [...this.tasks, task]
     },
--- vue/vue-frontend/src/components/Header.vue
+++ vue/vue-frontend/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button text="Add Task" color="green"/>
+        <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/>
     </header>
 </template>
 
@@ -14,11 +14,12 @@ export default {
         title: {
             type: String,
             default: "Hello"
-        }
+        },
+        showAddTask: Boolean
     },
     components: {
         Button,
-    }
+    },
 }
 </script>
 
