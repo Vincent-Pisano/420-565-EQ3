781bb8e61278fc2845f0cea0b9ab49be83a3664d
Pravdin Maksym <maks111111112@gmail.com>
2021-11-01 19:36:39 -0400

    Veille_Maksym Ajout de router

--- vue/vue-frontend/src/App.vue
+++ vue/vue-frontend/src/App.vue
@@ -6,12 +6,15 @@
       <AddTask @add-task="addTask" />
     </div>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+    <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 import Tasks from './components/Tasks.vue'
 import AddTask from './components/AddTask.vue'
 
@@ -21,6 +24,7 @@ export default {
     Header,
     Tasks,
     AddTask,
+    Footer,
   },
   data() {
     return {
---
+++ vue/vue-frontend/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>Ceci est le Home</h3>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  inheritAttrs: false,
+};
+</script>
