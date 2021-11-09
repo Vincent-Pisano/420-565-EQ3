2c20529de01de715c75161cad70a3288c7bc88a0
Pravdin Maksym <maks111111112@gmail.com>
2021-11-09 16:56:48 -0500

    EQ3-245 Fix de session hiver

--- backend/src/main/java/com/eq3/backend/utils/Utils.java
+++ backend/src/main/java/com/eq3/backend/utils/Utils.java
@@ -76,6 +76,6 @@ public class Utils {
         cal.setTime(date);
         int month = cal.get(Calendar.MONTH);
         int year = cal.get(Calendar.YEAR);
-        return month > 2 && month < 8 ? year + SUMMER_SESSION : year + WINTER_SESSION;
+        return month > 2 && month < 8 ? year + SUMMER_SESSION : (year + 1) + WINTER_SESSION;
     }
 }
