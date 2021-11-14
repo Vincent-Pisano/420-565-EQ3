34aecda906339cf465f86e60fb96a706e2f66f14
Pravdin Maksym <maks111111112@gmail.com>
2021-11-14 18:06:42 -0500

    Veille_Maksym Ajout de redirection si l'utilisateur n'est pas conncté

--- vue/vue-frontend/src/views/ProfilePage.vue
+++ vue/vue-frontend/src/views/ProfilePage.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="container">
-    <div class="card">
-      <img src="./../assets/pfp.png"  style="width: 100%" />
-      <h1>Nom d'utilisateur: {{this.user.username}}</h1>
-      <h2>Prénom, Nom: {{this.user.firstName}} {{this.user.lastName}}</h2>
-      <p class="title">Courriel: {{this.user.email}}</p>
-      <div v-if="this.user.username.startsWith('E') || this.user.username.startsWith('S')">
-        <p>Department: {{this.user.department}}</p>
+  <div v-if="this.user != null">
+    <div class="container">
+      <div class="card">
+        <img src="./../assets/pfp.png"  style="width: 100%" />
+        <h1>Nom d'utilisateur: {{this.user.username}}</h1>
+        <h2>Prénom, Nom: {{this.user.firstName}} {{this.user.lastName}}</h2>
+        <p class="title">Courriel: {{this.user.email}}</p>
+        <div v-if="this.user.username.startsWith('E') || this.user.username.startsWith('S')">
+          <p>Department: {{this.user.department}}</p>
+        </div>
+        <div v-if="this.user.username.startsWith('M')">
+          <p>Nom d'entreprise: {{this.user.enterpriseName}}</p>
+          <p>Poste de travail: {{this.user.jobTitle}}</p>
+        </div>
+        <br>
+        <p><button @click="logOut()">Déconnecter</button></p>
       </div>
-      <div v-if="this.user.username.startsWith('M')">
-        <p>Nom d'entreprise: {{this.user.enterpriseName}}</p>
-        <p>Poste de travail: {{this.user.jobTitle}}</p>
-      </div>
-      <br>
-      <p><button>Déconnecter</button></p>
     </div>
   </div>
 </template>
@@ -25,14 +27,18 @@ export default {
   name: "ProfilePage",
   inheritAttrs: false,
   methods: {
+    logOut() {
+      router.push("/");
+    },
     getUserInfo: function () {
-      if(sessionStorage.getItem("user")){
+      console.log(sessionStorage.getItem("user"));
+      if(sessionStorage.getItem("user") !== null){
         var user = sessionStorage.getItem("user");
         var viewName = JSON.parse(user);
         console.log(viewName);
         return viewName;
       } else {
-        router.push("/");
+        this.logOut();
       }
     },
   },
